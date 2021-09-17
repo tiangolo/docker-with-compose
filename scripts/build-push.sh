7de53736dbfec4165ddf7a00fcd64888fb7aa16f
Sebastián Ramírez <tiangolo@gmail.com>
2021-09-17 18:39:07 +0200

    🐛 Fix deployment

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+basename="tiangolo/docker-with-compose"
+latest_tag="${basename}:latest"
+dated_tag="${basename}:$(date -I)"
+
 bash scripts/build.sh
+
+docker tag "$latest_tag" "$dated_tag"
+
 bash scripts/docker-login.sh
 
 docker push "$latest_tag"
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,5 @@ set -e
 
 basename="tiangolo/docker-with-compose"
 latest_tag="${basename}:latest"
-dated_tag="${basename}:$(date -I)"
 
 docker build -t "$latest_tag" .
-
-docker tag "$latest_tag" "$dated_tag"
